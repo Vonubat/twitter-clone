6f94cc413b7637e559cdb0dbe772dc690ece3ac9
Tabunov Egor <Vonubat@gmail.com>
2023-02-21 21:44:26 +0300

    feat: implement Cover component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@ export { Layout } from './layout/Layout';
 export { Avatar } from './ui/Avatar';
 export { Banner } from './ui/Banner';
 export { Button } from './ui/Button';
+export { Cover } from './ui/Cover';
 export { Search } from './ui/Search';
--- src/components/ui/Avatar.tsx
+++ src/components/ui/Avatar.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 import defaultAvatar from '../../assets/default_avatar.png';
 
 type Props = {
@@ -7,23 +5,11 @@ type Props = {
 };
 
 export const Avatar = ({ src }: Props): JSX.Element => {
-  const [isHovering, setIsHovering] = useState(false);
-
-  const handleMouseOver = (): void => {
-    setIsHovering(true);
-  };
-
-  const handleMouseOut = (): void => {
-    setIsHovering(false);
-  };
-
   return (
-    <div className="avatar__wrapper">
-      <img
-        className="w-[70px] h-[70px] sm:w-[110px] sm:h-[110px] md:w-[150px] md:h-[150px] lg:w-[210px] lg:h-[210px] rounded-full object-cover border-solid border-4 border-white"
-        src={src || defaultAvatar}
-        alt="User avatar"
-      />
-    </div>
+    <img
+      className="w-[70px] h-[70px] sm:w-[110px] sm:h-[110px] md:w-[150px] md:h-[150px] lg:w-[210px] lg:h-[210px] rounded-full object-cover border-solid border-4 border-white"
+      src={src || defaultAvatar}
+      alt="User avatar"
+    />
   );
 };

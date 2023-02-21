1d0578005265df44112b3e9cf2bd89b9fb51825d
Tabunov Egor <Vonubat@gmail.com>
2023-02-21 18:13:44 +0300

    feat: implement WelcomePage

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -11,3 +11,9 @@ export enum LogInMsg {
   cantFindUser = 'cant_find_user_with_such_username_or_password',
   success = 'success',
 }
+
+export enum TypingSvg {
+  guccitterIsWhat = 'https://readme-typing-svg.demolab.com?font=Fira+Code&weight=700&duration=5000&color=34495e&multiline=true&width=350&height=100&lines=Guccitter+is+what%E2%80%99s+happening+;and+what+people+are+talking;+about+right+now.',
+  lifeIsNotAbout = 'https://readme-typing-svg.demolab.com?font=Fira+Code&weight=700&duration=3000&color=34495e&multiline=true&width=300&height=70&lines=Life%E2%80%99s+not+about+a+job%2C;+it%E2%80%99s+about+purpose.',
+  weServe = 'https://readme-typing-svg.demolab.com?font=Fira+Code&weight=700&duration=2000&color=34495b&multiline=true&width=250&height=70&lines=We+serve+the+public+;conversation.',
+}

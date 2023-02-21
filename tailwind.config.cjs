064061e03bfd6d68fbe61d8f02a3745b1868f57c
Tabunov Egor <Vonubat@gmail.com>
2023-02-21 18:10:35 +0300

    feat: add some custom animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'welcome-bg': 'welcome-bg-keyframe 6s ease infinite',
+        append: 'append-keyframe 0.5s linear',
+      },
+      keyframes: {
+        'welcome-bg-keyframe': {
+          '0%, 100%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' },
+        },
+        'append-keyframe': {
+          from: { transform: 'scale(0)', opacity: 0 },
+          to: { transform: 'scale(1)', opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [],
 };

af3199702a0e9c394a243a8509676b1cb1a1d6cc
Tabunov Egor <Vonubat@gmail.com>
2023-02-21 18:10:15 +0300

    feat: add useDebounce hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
+export { useDebounce } from './useDebounce';
 export { useStateWithLocalStorage } from './useStateWithLocalStorage';
 export { TwitterContextProvider, useTwitter } from './useTwitter';

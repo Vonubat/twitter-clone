b773b7fec92814aa5f1e3ae3f94954c01fc80c23
Tabunov Egor <Vonubat@gmail.com>
2023-02-21 20:28:19 +0300

    feat: implement Banner component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { Layout } from './layout/Layout';
+export { Avatar } from './ui/Avatar';
+export { Banner } from './ui/Banner';
 export { Button } from './ui/Button';
 export { Search } from './ui/Search';

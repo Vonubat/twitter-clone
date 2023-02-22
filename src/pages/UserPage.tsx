f83468a899e9e137a09140a621f6a680d125461f
Tabunov Egor <Vonubat@gmail.com>
2023-02-23 00:39:47 +0300

    fix: style bugs

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -21,16 +21,16 @@ export const UserPage = (): JSX.Element => {
         <Avatar src={user.avatar} />
       </div>
       <ControlBar user={user} />
-      <div className="content__wrapper flex justify-evenly items-center md:items-start flex-col md:flex-row mt-3 md:mt-8 lg:mt-16 pb-5 gap-3">
-        <div className="user-info__container ">
+      <div className="content__wrapper flex items-center md:items-start flex-col md:flex-row mt-3 md:mt-8 lg:mt-16 pb-5 gap-8">
+        <div className="user-info__container ml-0 md:ml-[13%] ">
           <UserInfo user={user} />
         </div>
-        <div className="tweets__container flex flex-col gap-px max-w-lg p-2 md:p-0">
+        <div className="tweets__container flex flex-col gap-px max-w-lg p-2 md:p-0 ml-0 lg:ml-[10%]">
           {user.tweets.map(({ tweetId }, index) => (
             <Tweet key={tweetId} user={user} index={index} />
           ))}
         </div>
-        <div className="banner__container hidden lg:block">{!ownerId && <Banner />}</div>
+        <div className="banner__container hidden xl:block">{!ownerId && <Banner />}</div>
       </div>
     </main>
   );

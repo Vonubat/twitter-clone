e7e5cca38b06fbde0cdbda525f7c3162e4d169d2
Tabunov Egor <Vonubat@gmail.com>
2023-02-23 14:05:09 +0300

    feat: update ControlBar (add new controls)

--- src/components/ui/ControlBar.tsx
+++ src/components/ui/ControlBar.tsx
@@ -1,3 +1,4 @@
+import { useTwitter } from '../../hooks';
 import { IUser } from '../../types';
 
 type Props = {
@@ -5,13 +6,30 @@ type Props = {
 };
 
 export const ControlBar = ({ user }: Props): JSX.Element => {
+  const { ownerId } = useTwitter();
+
   return (
-    <div className="w-full h-12 bg-white flex items-center">
-      <div className="tweet-controls flex">
-        <div className="tweets-counter__wrapper flex flex-col items-center border-b-2 border-sky-500 translate-x-[calc(100vw/3)] w-[100px]">
-          <span className="text-black font-medium opacity-50">Tweets</span>
-          <span className="font-semibold text-sky-500">{user.tweets.length}</span>
+    <div className="w-full h-[50px] bg-white flex items-center">
+      <div className="controls flex translate-x-[calc(100vw/3)] gap-1 overflow-x-auto w-[65%]">
+        <div className="tweets-counter__wrapper flex flex-col items-center border-b-2 border-sky-500 min-w-[100px] max-w-[100px]">
+          <span className="text-black font-medium text-opacity-50">Tweets</span>
+          <span className="tweets-counter font-semibold text-sky-500">{user.tweets.length}</span>
         </div>
+        {ownerId && (
+          <button className="tweets-btn__add text-black font-medium text-opacity-50 text-center border-b-2 border-transparent active:border-sky-500 w-full min-w-[100px] max-w-[100px]">
+            Add new Tweet
+          </button>
+        )}
+        {ownerId && (
+          <button className="tweets-btn__change-avatar text-black font-medium text-opacity-50 text-center border-b-2 border-transparent active:border-sky-500 min-w-[100px] max-w-[100px]">
+            Change Avatar
+          </button>
+        )}
+        {ownerId && (
+          <button className="tweets-btn__change-cover text-black font-medium text-opacity-50 text-center border-b-2 border-transparent active:border-sky-500 min-w-[100px] max-w-[100px]">
+            Change Cover
+          </button>
+        )}
       </div>
     </div>
   );

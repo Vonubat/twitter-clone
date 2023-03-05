405515f8b1bbf3e96567cd5e2ce373b7b0a276ad
Tabunov Egor <Vonubat@gmail.com>
2023-03-05 22:08:39 +0300

    feat: implement tweets functionality

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,15 +1,17 @@
 import { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { skipToken } from '@reduxjs/toolkit/dist/query';
 
 import { Avatar, Banner, ControlBar, Cover, Loading, Tweet, UserInfo } from '../components';
 import { Path } from '../constants';
-import { useAppSelector, useGetUserQuery, userSelector } from '../redux';
+import { useAppSelector, useGetListOfUserTweetsQuery, useGetUserQuery, userSelector } from '../redux';
 
 export const UserPage = (): JSX.Element => {
   const { username } = useParams();
   const navigate = useNavigate();
   const { owner } = useAppSelector(userSelector);
   const { data: user, isError } = useGetUserQuery(username as string, { refetchOnMountOrArgChange: true });
+  const { data: tweets } = useGetListOfUserTweetsQuery(user?.userId ?? skipToken);
 
   useEffect(() => {
     if (isError) {
@@ -29,9 +31,7 @@ export const UserPage = (): JSX.Element => {
           <UserInfo user={user} />
         </div>
         <div className="tweets__container ml-0 flex max-w-lg flex-col gap-px p-2 md:p-0 lg:ml-[10%]">
-          {user.tweets.map((tweet) => (
-            <Tweet key={tweet.tweetId} tweet={tweet} />
-          ))}
+          {tweets && tweets.map((tweet) => <Tweet key={tweet.tweetId} tweet={tweet} />)}
         </div>
         <div className="banner__container hidden xl:block">{!owner && <Banner />}</div>
       </div>
--- src/redux/api/tweetsApi.ts
+++ src/redux/api/tweetsApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 import { BASE_URL } from '../../constants';
-import { ITweet, IUser } from '../../types';
+import { InputEditor, ITweet, IUser } from '../../types';
 
 export const tweetsApi = createApi({
   reducerPath: 'tweetsApi',
@@ -17,8 +17,48 @@ export const tweetsApi = createApi({
           url: userId,
         };
       },
+      transformResponse: (response: ITweet[]) =>
+        response.sort(({ date: dateA }, { date: dateB }) => {
+          return new Date(Date.parse(dateB)).getTime() - new Date(Date.parse(dateA)).getTime();
+        }),
+      providesTags: (result) =>
+        result ? [...result.map(({ tweetId }) => ({ type: 'Tweet' as const, tweetId })), 'Tweet'] : ['Tweet'],
+    }),
+    createNewTweet: builder.mutation<IUser, InputEditor>({
+      query: (data) => {
+        return {
+          url: '',
+          method: 'POST',
+          body: { text: data.contentTextarea },
+        };
+      },
+      invalidatesTags: ['Tweet'],
+    }),
+    updateTweet: builder.mutation<IUser, { tweetId: ITweet['tweetId'] } & InputEditor>({
+      query: (data) => {
+        return {
+          url: data.tweetId,
+          method: 'PUT',
+          body: { text: data.contentTextarea },
+        };
+      },
+      invalidatesTags: ['Tweet'],
+    }),
+    deleteTweet: builder.mutation<IUser, ITweet['tweetId']>({
+      query: (tweetId) => {
+        return {
+          url: tweetId,
+          method: 'DELETE',
+        };
+      },
+      invalidatesTags: ['Tweet'],
     }),
   }),
 });
 
-export const { useGetListOfUserTweetsQuery } = tweetsApi;
+export const {
+  useGetListOfUserTweetsQuery,
+  useCreateNewTweetMutation,
+  useUpdateTweetMutation,
+  useDeleteTweetMutation,
+} = tweetsApi;
